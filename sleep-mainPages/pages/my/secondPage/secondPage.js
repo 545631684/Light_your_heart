250a3dcf1f08318b3871264407b0309234bf18fd
zyh <545631684@qq.com>
2021-06-17 10:11:35 +0800

    20210617更新上午10点
    用户反馈提交功能的接口对接，以及提示文字后台调取显示

--- sleep-mainPages/pages/my/secondPage/secondPage.js
+++ sleep-mainPages/pages/my/secondPage/secondPage.js
@@ -1,11 +1,14 @@
 // sleep-mainPages/pages/my/secondPage/secondPage.js
+import request from "../../../../network/request"
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    email:'',
+    textarea:'',
+    ti: ['','']
   },
 
   /**
@@ -17,6 +20,17 @@ Page({
     })
     this.setData({
       title:options.title
+    })
+    // 获取后台提示语句
+    let _this = this
+    request("/feedback/config",'post',"",wx.getStorageSync('token')).then(res=>{
+      let ti = res.data.data.content.split("\r\n"), tidata = []
+      ti.map(s=>{
+        tidata.push(s.replace(/\s/g,""))
+      })
+      _this.setData({
+        ti:tidata
+      })
     })
   },
   clearStorage(){
@@ -38,6 +52,46 @@ Page({
   submit(){
     console.log('意见内容',this.data.textarea)
     console.log('邮箱',this.data.email)
+    let _this = this
+    setTimeout(function(){
+      if(!_this.data.textarea){
+        wx.showModal({
+          title: '提示',
+          content: '请填写反馈内容',
+          showCancel:false,
+          success (res) {
+          }
+        })
+        return
+      }
+      if(!_this.data.email || !(/^[A-Za-z0-9\u4e00-\u9fa5]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/.test(_this.data.email))){
+        wx.showModal({
+          title: '提示',
+          content: '邮箱不能为空或邮箱格式不正确',
+          showCancel:false,
+          success (res) {
+          }
+        })
+        return
+      }
+      wx.showLoading({})
+      request("/feedback/record",'post',{MemberCode:wx.getStorageSync('MemberCode'),content:_this.data.textarea,email:_this.data.email},wx.getStorageSync('token')).then(res=>{
+        wx.hideLoading({})
+        wx.showModal({
+          title: '提示',
+          content: res.data.data,
+          showCancel:false,
+          success (res) {
+            // 提交成功后清空提交内容
+            _this.setData({
+              email:'',
+              textarea:'',
+            })
+          }
+        })
+      })
+    },500)
+    
   },
   /**
    * 生命周期函数--监听页面初次渲染完成
